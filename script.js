25f9115f69fcef117e49464a89c4a187b700dec0
Leonel Ivan Cardoso <cardosoleonel152@gmail.com>
2022-11-11 13:41:01 -0300

    Ultima actualización

--- script.js
+++ script.js
@@ -4,14 +4,36 @@ function initMap() {
     const uluru = { lat: -35.66875078858062, lng: -63.74478092325523 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 13,
-      center: uluru,
+        zoom: 13,
+        center: uluru,
     });
     // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
+        position: uluru,
+        map: map,
     });
-  }
-  
-  window.initMap = initMap;
+}
+
+window.initMap = initMap;
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
